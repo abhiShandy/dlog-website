527a04fa2b2825720630f8688cee4a9cafb9f6a7
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2023-07-08 10:48:27 -0400

    simple website

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,68 @@
+export default function PrivacyPolicy() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Privacy Policy</h1>
+      <p>
+        Effective Date: July 8, 2023
+        <br />
+        This Privacy Policy describes how D-Log (&quot;we,&quot; &quot;us,&quot;
+        or &quot;our&quot;) collects, uses, and protects the information you
+        provide while using our mobile application D-Log (the &quot;App&quot;).
+        We are committed to respecting and protecting your privacy and ensuring
+        the security of any personal information you may provide to us.
+      </p>
+      <ol className="list-decimal">
+        <li>
+          Information We Don&apos;t Collect We want to assure you that D-Log
+          does not collect any personally identifiable information (PII) or any
+          other data from you. We designed our App with privacy in mind, and we
+          do not require or request any personal information, such as your name,
+          email address, phone number, or location.
+        </li>
+        <li>
+          Automatically Collected Information When you use our App, we do not
+          automatically collect any information about you or your device. We do
+          not use cookies, log files, or any similar technologies to track or
+          gather data.
+        </li>
+        <li>
+          Links to Third-Party Websites Our App may contain links to third-party
+          websites or services that are not operated by us. We are not
+          responsible for the privacy practices or the content of those
+          third-party websites or services. We encourage you to review the
+          privacy policies of any third-party websites or services that you
+          visit.
+        </li>
+        <li>
+          Children&apos;s Privacy Our App is not intended for children under the
+          age of 13. We do not knowingly collect any personal information from
+          children under the age of 13. If you believe that we may have
+          collected information from a child under 13, please contact us
+          immediately, and we will promptly remove any such information from our
+          records.
+        </li>
+        <li>
+          Security We prioritize the security of your information. While we do
+          not collect any personal data, we use industry-standard security
+          measures to protect the limited information we have access to.
+          However, please note that no method of transmission over the internet
+          or electronic storage is completely secure, and we cannot guarantee
+          the absolute security of any information you transmit to us.
+        </li>
+        <li>
+          Changes to this Privacy Policy We reserve the right to modify or
+          update this Privacy Policy at any time. Any changes will be effective
+          upon posting the revised policy on our App. We encourage you to review
+          this Privacy Policy periodically for any updates or changes.
+        </li>
+      </ol>
+      <p>
+        Contact Us If you have any questions or concerns about this Privacy
+        Policy or our practices regarding your privacy, please contact us at
+        abhishek.sandy@gmail.com. By using our App, you acknowledge that you
+        have read and understood this Privacy Policy and agree to its terms and
+        conditions.
+      </p>
+    </main>
+  );
+}
